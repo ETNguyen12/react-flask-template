179cdc298d6da69cdef482497d00df8b528b5d49
Ethan Nguyen <nguyeeth@tamu.edu>
2024-11-28 00:24:37 -0600

    Fix serve react backend app for deployment

--- backend/run.py
+++ backend/run.py
@@ -3,16 +3,14 @@ from api import register_extensions, register_blueprints
 from api.config import Config
 import os
 
-# Initialize the Flask app
-app = Flask(__name__)
-app.config.from_object(Config)
-
-# Register extensions and blueprints
-register_extensions(app)
-register_blueprints(app)
-
 # Serve React app only in production
 if os.getenv("FLASK_ENV") == "production":
+    app = Flask(__name__, static_folder='build', static_url_path='')
+    app.config.from_object(Config)
+
+    register_extensions(app)
+    register_blueprints(app)
+
     @app.route('/')
     def serve():
         return send_from_directory(app.static_folder, 'index.html')
@@ -20,6 +18,14 @@ if os.getenv("FLASK_ENV") == "production":
     @app.errorhandler(404)
     def not_found(e):
         return send_from_directory(app.static_folder, 'index.html')
+    
+# Regular flask app in development
+else: 
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    register_extensions(app)
+    register_blueprints(app)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001, debug=True)
